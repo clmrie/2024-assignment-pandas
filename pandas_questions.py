e508865e5d4ca438c95254b4a0ffe1e9f10ad791
clmrie <marieclement172@gmail.com>
2025-01-02 15:22:10 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 def load_data():
     """Load data from the CSV files referendum/regions/departments."""
@@ -16,12 +16,16 @@ def load_data():
 
     return referendum, regions, departments
 
+
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame."""
     # Remove leading zeros
-    
+
     merged = departments.merge(
-        regions, left_on="region_code", right_on="code", suffixes=("_dep", "_reg")
+        regions,
+        left_on="region_code",
+        right_on="code",
+        suffixes=("_dep", "_reg")
     )
     merged = merged.rename(columns={
         "region_code": "code_reg",
@@ -29,7 +33,7 @@ def merge_regions_and_departments(regions, departments):
         "code_dep": "code_dep",
         "name_dep": "name_dep"
     })
-    
+
     merged = merged.loc[:, ~merged.columns.duplicated()]
     return merged[["code_reg", "name_reg", "code_dep", "name_dep"]]
 
@@ -40,12 +44,21 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-    regions_and_departments["code_dep"] = regions_and_departments["code_dep"].replace({
-        "01": "1", "02": "2", "03": "3", "04": "4", 
-        "05": "5", "06": "6", "07": "7", "08": "8", "09": "9"
+    regions_and_departments["code_dep"] = regions_and_departments[
+        "code_dep"
+    ].replace({
+        "01": "1",
+        "02": "2",
+        "03": "3",
+        "04": "4",
+        "05": "5",
+        "06": "6",
+        "07": "7",
+        "08": "8",
+        "09": "9"
     })
-   
-    #referendum = referendum[referendum["Department code"].str.isnumeric()]
+
+    # referendum = referendum[referendum["Department code"].str.isnumeric()]
     merged = referendum.merge(
         regions_and_departments,
         left_on="Department code",
@@ -54,18 +67,24 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     )
     return merged
 
+
 def compute_referendum_result_by_regions(referendum_and_areas):
     """Return a table with the absolute count for each region.
 
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    grouped = referendum_and_areas.groupby(["code_reg", "name_reg"]).sum().reset_index()
+    grouped = referendum_and_areas.groupby(
+        ["code_reg", "name_reg"]
+    ).sum().reset_index()
 
-    #print(grouped[["code_reg", "name_reg", "Registered", "Abstentions", "Null", "Choice A", "Choice B"]].head())
     grouped = grouped.drop(columns=['code_reg'], axis=1)
-    grouped = grouped[[ "name_reg", "Registered", "Abstentions", "Null", "Choice A", "Choice B"]]
+    grouped = grouped[
+        ["name_reg", "Registered", "Abstentions",
+            "Null", "Choice A", "Choice B"]
+    ]
     return grouped
+
 
 def plot_referendum_map(referendum_result_by_regions):
     """Plot a map with the results from the referendum.
@@ -81,10 +100,16 @@ def plot_referendum_map(referendum_result_by_regions):
     print(geo_regions.head())
     print(referendum_result_by_regions.head())
 
-    merged = geo_regions.merge(referendum_result_by_regions, left_on="nom", right_on="name_reg")
+    merged = geo_regions.merge(
+        referendum_result_by_regions,
+        left_on="nom",
+        right_on="name_reg"
+    )
 
-    merged["ratio"] = merged["Choice A"] / (merged["Choice A"] + merged["Choice B"])
-    
+    merged["ratio"] = merged["Choice A"] / (
+        merged["Choice A"] + merged["Choice B"]
+    )
+
     merged.rename(columns={'nom': 'name_reg'}, inplace=True)
 
     merged.drop(['name_reg_x'], inplace=True, axis=1)
@@ -92,16 +117,17 @@ def plot_referendum_map(referendum_result_by_regions):
     merged.plot(column="ratio", legend=True, cmap="coolwarm")
     return merged
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     referendum, df_reg, df_dep = load_data()
     print("ref shape", referendum.shape)
     regions_and_departments = merge_regions_and_departments(df_reg, df_dep)
     print("reg dep shape", regions_and_departments.shape)
-    referendum_and_areas = merge_referendum_and_areas(referendum, regions_and_departments)
+    referendum_and_areas = merge_referendum_and_areas(referendum,
+                                                      regions_and_departments)
     print("ref and areas ", referendum_and_areas.shape)
-    referendum_results = compute_referendum_result_by_regions(referendum_and_areas)
-
+    referendum_results = \
+        compute_referendum_result_by_regions(referendum_and_areas)
 
     plot_referendum_map(referendum_results)
     plt.show()
